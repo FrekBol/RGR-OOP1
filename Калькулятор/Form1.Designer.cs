e04b9d3973731e91164717fc18e4f02dca95d009
FrekBol <129262560+FrekBol@users.noreply.github.com>
2024-06-24 13:05:05 +0300

    Add files via upload

---
+++ Калькулятор/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace ExpressionEvaluator
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.txtExpression = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+      
+            this.txtExpression.Location = new System.Drawing.Point(12, 12);
+            this.txtExpression.Name = "txtExpression";
+            this.txtExpression.Size = new System.Drawing.Size(260, 20);
+            this.txtExpression.TabIndex = 0;
+        
+            this.btnCalculate.Location = new System.Drawing.Point(12, 38);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 1;
+            this.btnCalculate.Text = "Обчислити";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+          
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(12, 70);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(62, 13);
+            this.lblResult.TabIndex = 2;
+            this.lblResult.Text = "Результат:";
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 101);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtExpression);
+            this.Name = "Form1";
+            this.Text = "Обчислювач виразів";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.TextBox txtExpression;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
---
+++ Калькулятор/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace ExpressionEvaluator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            string expression = txtExpression.Text;
+            try
+            {
+                double result = EvaluateExpression(expression);
+                lblResult.Text = $"Результат: {result}";
+            }
+            catch (Exception ex)
+            {
+                lblResult.Text = $"Помилка: {ex.Message}";
+            }
+        }
+
+        private double EvaluateExpression(string expression)
+        {
+            expression = expression.Replace(" ", "");
+            var tokens = Regex.Split(expression, @"([+\-*/])");
+            var numbers = new Stack<double>();
+            var operators = new Stack<char>();
+
+            foreach (var token in tokens)
+            {
+                if (double.TryParse(token, NumberStyles.Any, CultureInfo.InvariantCulture, out double number))
+                {
+                    numbers.Push(number);
+                }
+                else if (IsOperator(token[0]))
+                {
+                    while (operators.Count > 0 && Precedence(operators.Peek()) >= Precedence(token[0]))
+                    {
+                        ProcessOperation(numbers, operators);
+                    }
+                    operators.Push(token[0]);
+                }
+            }
+
+            while (operators.Count > 0)
+            {
+                ProcessOperation(numbers, operators);
+            }
+
+            return numbers.Pop();
+        }
+
+        private bool IsOperator(char c)
+        {
+            return c == '+' || c == '-' || c == '*' || c == '/';
+        }
+
+        private int Precedence(char op)
+        {
+            switch (op)
+            {
+                case '+':
+                case '-':
+                    return 1;
+                case '*':
+                case '/':
+                    return 2;
+                default:
+                    return 0;
+            }
+        }
+
+        private void ProcessOperation(Stack<double> numbers, Stack<char> operators)
+        {
+            char op = operators.Pop();
+            double right = numbers.Pop();
+            double left = numbers.Pop();
+            double result = ApplyOperation(left, right, op);
+            numbers.Push(result);
+        }
+
+        private double ApplyOperation(double left, double right, char op)
+        {
+            switch (op)
+            {
+                case '+':
+                    return left + right;
+                case '-':
+                    return left - right;
+                case '*':
+                    return left * right;
+                case '/':
+                    if (right == 0)
+                    {
+                        throw new DivideByZeroException("Ділення на нуль");
+                    }
+                    return left / right;
+                default:
+                    throw new InvalidOperationException("Невідома операція");
+            }
+        }
+    }
+}
